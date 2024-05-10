e416d76fee0c452cbc4e4e057daa8573af49cfd2
amandaklein-kd <60615828+amandakklein@users.noreply.github.com>
2024-05-10 16:39:02 -0300

    correcoes e arrumei a nav

--- site.js
+++ site.js
@@ -1,5 +1,18 @@
-$(document).ready(function(){
-    var hash = window.location.hash;
-    $('.navbar-nav a[href="'+hash+'"]').addClass('active');
-
-});
+$(document).ready(function() {
+    // Função para adicionar a classe 'active' ao item do menu correspondente à seção visível na página
+    $(window).on('scroll', function() {
+      var scrollPos = $(window).scrollTop();
+      $('.navbar-nav a').each(function() {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        var offset = 100; // ajuste conforme necessário
+        if (refElement.position().top - offset <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+          $('.navbar-nav a').removeClass("active");
+          currLink.addClass("active");
+        } else {
+          currLink.removeClass("active");
+        }
+      });
+    });
+  });
+  
